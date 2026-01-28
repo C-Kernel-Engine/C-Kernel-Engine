d75076cbb00e03ffca218aeb5333c26781b29e8e
antshiv <antshiv@users.noreply.github.com>
2026-01-28 15:02:33 +0000

    deploy: 9777496a399fb174ee0f84a3ab644517b97899b2

--- doxygen/search/defines_e.js
+++ doxygen/search/defines_e.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['pack_5fthreshold_4607',['PACK_THRESHOLD',['../gemm__microkernel_8c.html#a757a5b148b758bc38cbc87a99d33de9f',1,'gemm_microkernel.c']]],
-  ['prefill_5ftile_5fm_4608',['PREFILL_TILE_M',['../prefill__fused__gemm_8c.html#a27912bbb68970a254a4222e66be7bf98',1,'prefill_fused_gemm.c']]],
-  ['prefill_5ftile_5fn_4609',['PREFILL_TILE_N',['../prefill__fused__gemm_8c.html#a77d5e9fd2c94d63935abf32e5584251b',1,'prefill_fused_gemm.c']]]
+  ['pack_5fthreshold_0',['PACK_THRESHOLD',['../gemm__microkernel_8c.html#a757a5b148b758bc38cbc87a99d33de9f',1,'gemm_microkernel.c']]],
+  ['prefill_5ftile_5fm_1',['PREFILL_TILE_M',['../prefill__fused__gemm_8c.html#a27912bbb68970a254a4222e66be7bf98',1,'prefill_fused_gemm.c']]],
+  ['prefill_5ftile_5fn_2',['PREFILL_TILE_N',['../prefill__fused__gemm_8c.html#a77d5e9fd2c94d63935abf32e5584251b',1,'prefill_fused_gemm.c']]]
 ];
--- doxygen/search/files_0.js
+++ doxygen/search/files_0.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['add_5fkernels_5fbf16_2ec_2401',['add_kernels_bf16.c',['../add__kernels__bf16_8c.html',1,'']]],
-  ['attention_5fdecode_5ffused_2ec_2402',['attention_decode_fused.c',['../attention__decode__fused_8c.html',1,'']]],
-  ['attention_5fflash_5ftrue_2ec_2403',['attention_flash_true.c',['../attention__flash__true_8c.html',1,'']]],
-  ['attention_5fkernels_2ec_2404',['attention_kernels.c',['../attention__kernels_8c.html',1,'']]],
-  ['attention_5fmlp_5ffused_2ec_2405',['attention_mlp_fused.c',['../attention__mlp__fused_8c.html',1,'']]],
-  ['axpy_5fkernels_2ec_2406',['axpy_kernels.c',['../axpy__kernels_8c.html',1,'']]]
+  ['add_5fkernels_5fbf16_2ec_0',['add_kernels_bf16.c',['../add__kernels__bf16_8c.html',1,'']]],
+  ['attention_5fdecode_5ffused_2ec_1',['attention_decode_fused.c',['../attention__decode__fused_8c.html',1,'']]],
+  ['attention_5fflash_5ftrue_2ec_2',['attention_flash_true.c',['../attention__flash__true_8c.html',1,'']]],
+  ['attention_5fkernels_2ec_3',['attention_kernels.c',['../attention__kernels_8c.html',1,'']]],
+  ['attention_5fmlp_5ffused_2ec_4',['attention_mlp_fused.c',['../attention__mlp__fused_8c.html',1,'']]],
+  ['axpy_5fkernels_2ec_5',['axpy_kernels.c',['../axpy__kernels_8c.html',1,'']]]
 ];
