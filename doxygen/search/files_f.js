b64c03b02b571716b8e23e0799c940087801c4ae
antshiv <antshiv@users.noreply.github.com>
2026-01-31 14:32:10 +0000

    deploy: fb22347d17a1ac4a2a7d58595a81c93ab6887e50

--- doxygen/search/files_f.js
+++ doxygen/search/files_f.js
@@ -1,13 +1,13 @@
 var searchData=
 [
-  ['relu_5fkernels_2ec_0',['relu_kernels.c',['../relu__kernels_8c.html',1,'']]],
-  ['relu_5fkernels_5fbf16_2ec_1',['relu_kernels_bf16.c',['../relu__kernels__bf16_8c.html',1,'']]],
-  ['rmsnorm_5fkernels_2ec_2',['rmsnorm_kernels.c',['../rmsnorm__kernels_8c.html',1,'']]],
-  ['rmsnorm_5fkernels_5fbf16_2ec_3',['rmsnorm_kernels_bf16.c',['../rmsnorm__kernels__bf16_8c.html',1,'']]],
-  ['rmsnorm_5fkernels_5fint4_2ec_4',['rmsnorm_kernels_int4.c',['../rmsnorm__kernels__int4_8c.html',1,'']]],
-  ['rmsnorm_5fkernels_5fint8_2ec_5',['rmsnorm_kernels_int8.c',['../rmsnorm__kernels__int8_8c.html',1,'']]],
-  ['rmsnorm_5fq8_5fk_5ffused_2ec_6',['rmsnorm_q8_k_fused.c',['../rmsnorm__q8__k__fused_8c.html',1,'']]],
-  ['rmsnorm_5fqkv_2ec_7',['rmsnorm_qkv.c',['../rmsnorm__qkv_8c.html',1,'']]],
-  ['rope_5fkernels_2ec_8',['rope_kernels.c',['../rope__kernels_8c.html',1,'']]],
-  ['rope_5fkernels_5fbf16_2ec_9',['rope_kernels_bf16.c',['../rope__kernels__bf16_8c.html',1,'']]]
+  ['relu_5fkernels_2ec_2587',['relu_kernels.c',['../relu__kernels_8c.html',1,'']]],
+  ['relu_5fkernels_5fbf16_2ec_2588',['relu_kernels_bf16.c',['../relu__kernels__bf16_8c.html',1,'']]],
+  ['rmsnorm_5fkernels_2ec_2589',['rmsnorm_kernels.c',['../rmsnorm__kernels_8c.html',1,'']]],
+  ['rmsnorm_5fkernels_5fbf16_2ec_2590',['rmsnorm_kernels_bf16.c',['../rmsnorm__kernels__bf16_8c.html',1,'']]],
+  ['rmsnorm_5fkernels_5fint4_2ec_2591',['rmsnorm_kernels_int4.c',['../rmsnorm__kernels__int4_8c.html',1,'']]],
+  ['rmsnorm_5fkernels_5fint8_2ec_2592',['rmsnorm_kernels_int8.c',['../rmsnorm__kernels__int8_8c.html',1,'']]],
+  ['rmsnorm_5fq8_5fk_5ffused_2ec_2593',['rmsnorm_q8_k_fused.c',['../rmsnorm__q8__k__fused_8c.html',1,'']]],
+  ['rmsnorm_5fqkv_2ec_2594',['rmsnorm_qkv.c',['../rmsnorm__qkv_8c.html',1,'']]],
+  ['rope_5fkernels_2ec_2595',['rope_kernels.c',['../rope__kernels_8c.html',1,'']]],
+  ['rope_5fkernels_5fbf16_2ec_2596',['rope_kernels_bf16.c',['../rope__kernels__bf16_8c.html',1,'']]]
 ];
