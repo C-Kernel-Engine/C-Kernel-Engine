ef17d6b4fc78d574e35185ae787f9c5813ca2473
antshiv <antshiv@users.noreply.github.com>
2026-02-07 15:52:34 +0000

    deploy: cf800321c1a1a42fbc2c4079b6e91e92de696813

--- doxygen/search/files_e.js
+++ doxygen/search/files_e.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['quantize_5frow_5fq8_5fk_5fsse_2ec_2586',['quantize_row_q8_k_sse.c',['../quantize__row__q8__k__sse_8c.html',1,'']]]
+  ['qk_5fnorm_5fkernels_2ec_0',['qk_norm_kernels.c',['../qk__norm__kernels_8c.html',1,'']]],
+  ['quantize_5frow_5fq8_5fk_5fsse_2ec_1',['quantize_row_q8_k_sse.c',['../quantize__row__q8__k__sse_8c.html',1,'']]]
 ];
---
+++ doxygen/structblock__q5__K.js
@@ -0,0 +1,8 @@
+var structblock__q5__K =
+[
+    [ "d", "structblock__q5__K.html#a27a6395079d228e587d2eef92b0feb1f", null ],
+    [ "dmin", "structblock__q5__K.html#a07f951427f551959727129b23739d0ff", null ],
+    [ "qh", "structblock__q5__K.html#a56725854491cd63ff84a8c9144c774d1", null ],
+    [ "qs", "structblock__q5__K.html#a6f9a15b4156d1a87d2256452292abaa8", null ],
+    [ "scales", "structblock__q5__K.html#a9597443d7e1be5dc8d8a08a1f3f9226c", null ]
+];
