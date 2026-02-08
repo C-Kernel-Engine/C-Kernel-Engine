0b5b30d1bfa41d39679193c2471ea9b1231bab12
antshiv <antshiv@users.noreply.github.com>
2026-02-08 23:42:47 +0000

    deploy: 9bac8bbd86b90ab0c054761bc49e59d8bc90e955

--- doxygen/gemm__kernels__q5__1__q8__1_8c.js
+++ doxygen/gemm__kernels__q5__1__q8__1_8c.js
@@ -1,6 +1,7 @@
 var gemm__kernels__q5__1__q8__1_8c =
 [
     [ "CK_Q51_STACK_Q8_BLOCKS", "gemm__kernels__q5__1__q8__1_8c.html#ac4283ad44999c29ed0867b234557a697", null ],
+    [ "QK8_1", "gemm__kernels__q5__1__q8__1_8c.html#a60211474cab282bcfc7bd3c2aea64af7", null ],
     [ "dot_q5_1_q8_1_block", "gemm__kernels__q5__1__q8__1_8c.html#af38ec26b97b6fcf2813351abcbf4fa0a", null ],
     [ "gemm_nt_q5_1_q8_1", "gemm__kernels__q5__1__q8__1_8c.html#a60cd30d33fd29a3727b6299f0ac2e3d2", null ],
     [ "gemm_nt_q5_1_q8_1_ref", "gemm__kernels__q5__1__q8__1_8c.html#a3ba28e59408c4d585891a14e8c2bb6ef", null ],
--- doxygen/structCKTokenizerConfig.js
+++ doxygen/structCKTokenizerConfig.js
@@ -2,11 +2,9 @@ var structCKTokenizerConfig =
 [
     [ "add_bos", "structCKTokenizerConfig.html#a1c6c52e64b87d70be97a2e9547f950e5", null ],
     [ "add_eos", "structCKTokenizerConfig.html#adf662a4222984144966097373043a51f", null ],
-    [ "add_space_prefix", "structCKTokenizerConfig.html#a6318844b8b3efb4e6e5e352f1967c4a9", null ],
     [ "lowercase", "structCKTokenizerConfig.html#ad33c5d657dea175cb8c1ae9fdff33816", null ],
     [ "space_prefix_detected", "structCKTokenizerConfig.html#af82fde6580003dff44ed719380e61673", null ],
     [ "space_prefix_style", "structCKTokenizerConfig.html#af8f0acf8af0468b0c852db59df1172bb", null ],
-    [ "spm_mode", "structCKTokenizerConfig.html#ae2fe2d16420d6118bc884b3f0ef17e4d", null ],
     [ "treat_whitespace_as_suffix", "structCKTokenizerConfig.html#a61cf1a1974d46e1da99c1c86f43fad4b", null ],
     [ "type", "structCKTokenizerConfig.html#adeee37aac61b482ef54dc4262852a035", null ],
     [ "unk_score", "structCKTokenizerConfig.html#a25de8175fe44fc209d958136ce3c669e", null ],
