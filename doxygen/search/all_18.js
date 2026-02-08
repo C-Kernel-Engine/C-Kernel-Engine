91d5c8f31e6ea7c0fb1f6af5d2daaaa508fe5b0a
antshiv <antshiv@users.noreply.github.com>
2026-02-08 18:30:25 +0000

    deploy: 6a2da9ecfacab8b8e11490efb76aa33073ec49d6

--- doxygen/search/all_18.js
+++ doxygen/search/all_18.js
@@ -1,4 +1,17 @@
 var searchData=
 [
-  ['yellow_2449',['YELLOW',['../show__config_8c.html#abf681265909adf3d3e8116c93c0ba179',1,'show_config.c']]]
+  ['unfused_5frmsnorm_5flinear_5fq4k_5fref_0',['unfused_rmsnorm_linear_q4k_ref',['../fused__rmsnorm__linear_8c.html#a76e2f43c7d16e942259cde5c393da466',1,'fused_rmsnorm_linear.c']]],
+  ['unfused_5frmsnorm_5fqkv_5fprefill_1',['unfused_rmsnorm_qkv_prefill',['../ckernel__engine_8h.html#a96082b299d4fa9b2e3cfcd6077b22ca3',1,'unfused_rmsnorm_qkv_prefill(const float *x, const float *gamma, const float *Wq, const float *Wk, const float *Wv, float *x_norm, float *Q, float *K, float *V, int seq_len, int hidden, int q_dim, int kv_dim, float eps):&#160;prefill_fused_gemm.c'],['../prefill__fused__gemm_8c.html#a96082b299d4fa9b2e3cfcd6077b22ca3',1,'unfused_rmsnorm_qkv_prefill(const float *x, const float *gamma, const float *Wq, const float *Wk, const float *Wv, float *x_norm, float *Q, float *K, float *V, int seq_len, int hidden, int q_dim, int kv_dim, float eps):&#160;prefill_fused_gemm.c']]],
+  ['unk_2',['unk',['../tokenizer_8h.html#a9b857f7ce4dd3bca5a537e67f77a2b44',1,'unk:&#160;tokenizer.h'],['../true__bpe_8h.html#a9b857f7ce4dd3bca5a537e67f77a2b44',1,'unk:&#160;true_bpe.h']]],
+  ['unk_5fid_3',['unk_id',['../structCKTokenizer.html#a35f73551a504361ab117452f40fe81bf',1,'CKTokenizer']]],
+  ['unk_5fscore_4',['unk_score',['../structCKTokenizerConfig.html#a25de8175fe44fc209d958136ce3c669e',1,'CKTokenizerConfig']]],
+  ['unpack_5fq4_5fk_5fscales_5',['unpack_q4_k_scales',['../ckernel__quant_8h.html#adc037213865781c5c1ea63708ef94307',1,'ckernel_quant.h']]],
+  ['use_5fcolors_6',['use_colors',['../show__config_8c.html#a7d98e2dd1964a2c9cdb897cc63f23d0b',1,'show_config.c']]],
+  ['use_5ftrie_7',['use_trie',['../structCKTokenizerConfig.html#a6a4c7a490b1e83f6dfc73b351b3826ec',1,'CKTokenizerConfig::use_trie'],['../tokenizer_8h.html#abba2eff6b3bd59ba528a2828b636dc15',1,'use_trie:&#160;tokenizer.h']]],
+  ['used_8',['used',['../structCKPoolBlock.html#ad15144404362a0676f0c46a5b5a8d0ba',1,'CKPoolBlock::used'],['../structCKTokenizerMemPool.html#a390bbe0c2c3c9c2dcc7f5fc14d8c587f',1,'CKTokenizerMemPool::used']]],
+  ['used_20in_20v6_206_9',['used in v6 6',['../ckernel__orchestration_8c.html#autotoc_md15',1,'LEGACY CODE - NOT USED IN v6.6'],['../ckernel__codegen_8c.html#autotoc_md12',1,'LEGACY CODE - NOT USED IN v6.6'],['../ckernel__orchestration_8h.html#autotoc_md3',1,'LEGACY HEADER - NOT USED IN v6.6']]],
+  ['utf8_2ec_10',['utf8.c',['../utf8_8c.html',1,'']]],
+  ['utf8_2eh_11',['utf8.h',['../utf8_8h.html',1,'']]],
+  ['utf8_5fchar_5flen_12',['utf8_char_len',['../true__bpe_8c.html#a75cb6719be3c165611a4b97047856b6b',1,'true_bpe.c']]],
+  ['utf8_5flen_13',['utf8_len',['../tokenizer__spm_8c.html#a8ac84844873197352834b51dd6e8dbb8',1,'tokenizer_spm.c']]]
 ];
