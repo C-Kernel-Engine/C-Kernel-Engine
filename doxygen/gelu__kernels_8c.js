8b6c8034bad7dcf52cce6f14e6fc06b119ebac3c
antshiv <antshiv@users.noreply.github.com>
2026-02-09 18:38:24 +0000

    deploy: 3cace2154b4d1875e6b00317655e4f8e5cec8145

--- doxygen/gelu__kernels_8c.js
+++ doxygen/gelu__kernels_8c.js
@@ -1,5 +1,8 @@
 var gelu__kernels_8c =
 [
+    [ "geglu_backward_fp32", "gelu__kernels_8c.html#a44b759126bd6e478b5fc2720aa0dd2ea", null ],
+    [ "geglu_forward_bf16", "gelu__kernels_8c.html#ac2146f8009ce79891bd0b7d54e174992", null ],
+    [ "geglu_forward_fp32", "gelu__kernels_8c.html#a80b1ec81e751b153977f05a3e44b4fda", null ],
     [ "gelu_backward_exact", "gelu__kernels_8c.html#a3075b77764b4b408e3c1c9b89a2822ad", null ],
     [ "gelu_backward_fast", "gelu__kernels_8c.html#aeaf518a5239f4588f8c28b05b96d5478", null ],
     [ "gelu_backward_scalar", "gelu__kernels_8c.html#ac4d3711dee095194f24437af73ad1763", null ],
