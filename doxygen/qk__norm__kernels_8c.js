80e2abef3c2053388097d69e7615fe44fbd9dbea
antshiv <antshiv@users.noreply.github.com>
2026-02-20 17:12:22 +0000

    deploy: f81cf530c3321bd39973bef735629e27f975c4db

--- doxygen/qk__norm__kernels_8c.js
+++ doxygen/qk__norm__kernels_8c.js
@@ -1,5 +1,21 @@
 var qk__norm__kernels_8c =
 [
+    [ "QKNormISA", "qk__norm__kernels_8c.html#a11413abf502326b875f2afcbeabe0755", [
+      [ "QK_NORM_ISA_SCALAR", "qk__norm__kernels_8c.html#a11413abf502326b875f2afcbeabe0755ae8a33096f06a0b90497f7b81c5fc024f", null ],
+      [ "QK_NORM_ISA_AVX", "qk__norm__kernels_8c.html#a11413abf502326b875f2afcbeabe0755afae38bc9c5a9cf69e1a17e4ce09c54ac", null ],
+      [ "QK_NORM_ISA_AVX2", "qk__norm__kernels_8c.html#a11413abf502326b875f2afcbeabe0755a8ef9597b7f47de9209de4f8aa4e58368", null ],
+      [ "QK_NORM_ISA_AVX_VNNI", "qk__norm__kernels_8c.html#a11413abf502326b875f2afcbeabe0755a6dfe01b97d97594c7bcce0f35a87bb65", null ],
+      [ "QK_NORM_ISA_AUTO", "qk__norm__kernels_8c.html#a11413abf502326b875f2afcbeabe0755ab4f9a76a24692e46a07cf57d949b8b20", null ]
+    ] ],
+    [ "qk_norm_backward", "qk__norm__kernels_8c.html#a65aaed2762fb5c6959701a6f6971ea08", null ],
+    [ "qk_norm_backward_last_isa", "qk__norm__kernels_8c.html#ac094b4db531cfe6007a2a579eea9fa2e", null ],
+    [ "qk_norm_compute_rstd", "qk__norm__kernels_8c.html#a48e379366322fa62710bcaf06de7b816", null ],
+    [ "qk_norm_compute_rstd_scalar", "qk__norm__kernels_8c.html#a1510a0e84bb99232ccce7b8198d2855f", null ],
     [ "qk_norm_forward", "qk__norm__kernels_8c.html#a7520c628ab3cdfbb8b5d6303a2db125a", null ],
-    [ "rmsnorm_forward", "qk__norm__kernels_8c.html#a9a068ed02d809a81a23f09e61b820baf", null ]
+    [ "qk_norm_isa_compiled", "qk__norm__kernels_8c.html#a141fe803bb692418ec43cf1b21c9f765", null ],
+    [ "qk_norm_parse_forced_isa", "qk__norm__kernels_8c.html#ae67acb71f1be377608b0f2ccc63361c7", null ],
+    [ "qk_norm_select_isa", "qk__norm__kernels_8c.html#af86dc450313b9669b3fff9d6b3f01e71", null ],
+    [ "rmsnorm_backward", "qk__norm__kernels_8c.html#aca68d7dda1a7de9ff926920804751db0", null ],
+    [ "rmsnorm_forward", "qk__norm__kernels_8c.html#a9a068ed02d809a81a23f09e61b820baf", null ],
+    [ "g_qk_norm_last_isa", "qk__norm__kernels_8c.html#a9d288461faa85bf5221bd23ec71650f1", null ]
 ];
