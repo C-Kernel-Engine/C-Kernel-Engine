933ae7cc84f22e2ca5b54835c75ef24db4eea058
antshiv <antshiv@users.noreply.github.com>
2026-02-26 19:51:08 +0000

    deploy: 48f0529105b74e3c05b9282be8ad5abead508e50

--- doxygen/search/files_0.js
+++ doxygen/search/files_0.js
@@ -1,10 +1,9 @@
 var searchData=
 [
-  ['add_5fkernels_5fbf16_2ec_0',['add_kernels_bf16.c',['../add__kernels__bf16_8c.html',1,'']]],
-  ['attention_5fdecode_5ffused_2ec_1',['attention_decode_fused.c',['../attention__decode__fused_8c.html',1,'']]],
-  ['attention_5fflash_5ftrue_2ec_2',['attention_flash_true.c',['../attention__flash__true_8c.html',1,'']]],
-  ['attention_5fkernels_2ec_3',['attention_kernels.c',['../attention__kernels_8c.html',1,'']]],
-  ['attention_5fkernels_5fsliding_2ec_4',['attention_kernels_sliding.c',['../attention__kernels__sliding_8c.html',1,'']]],
-  ['attention_5fmlp_5ffused_2ec_5',['attention_mlp_fused.c',['../attention__mlp__fused_8c.html',1,'']]],
-  ['axpy_5fkernels_2ec_6',['axpy_kernels.c',['../axpy__kernels_8c.html',1,'']]]
+  ['add_5fkernels_5fbf16_2ec_2534',['add_kernels_bf16.c',['../add__kernels__bf16_8c.html',1,'']]],
+  ['attention_5fdecode_5ffused_2ec_2535',['attention_decode_fused.c',['../attention__decode__fused_8c.html',1,'']]],
+  ['attention_5fflash_5ftrue_2ec_2536',['attention_flash_true.c',['../attention__flash__true_8c.html',1,'']]],
+  ['attention_5fkernels_2ec_2537',['attention_kernels.c',['../attention__kernels_8c.html',1,'']]],
+  ['attention_5fmlp_5ffused_2ec_2538',['attention_mlp_fused.c',['../attention__mlp__fused_8c.html',1,'']]],
+  ['axpy_5fkernels_2ec_2539',['axpy_kernels.c',['../axpy__kernels_8c.html',1,'']]]
 ];
