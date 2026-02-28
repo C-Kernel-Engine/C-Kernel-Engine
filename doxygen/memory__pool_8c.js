2a1e4440bd76e7ac191cb35e795bd3728bcad9e6
antshiv <antshiv@users.noreply.github.com>
2026-02-28 18:39:31 +0000

    deploy: e656160d85a34dc8f3cb7b7d392f26e5a3fb5b67

--- doxygen/memory__pool_8c.js
+++ doxygen/memory__pool_8c.js
@@ -1,13 +1,13 @@
 var memory__pool_8c =
 [
     [ "align_up", "memory__pool_8c.html#a11253de42680a321ded636e9e3e0bb2c", null ],
-    [ "ck_tokenizer_mempool_alloc", "memory__pool_8c.html#ac13764c18fca9d95046a4fbc977cb7f4", null ],
-    [ "ck_tokenizer_mempool_alloc_aligned", "memory__pool_8c.html#a49b9bffa4f585a5b3b6205607188c0b1", null ],
+    [ "ck_tokenizer_mempool_alloc", "memory__pool_8c.html#ad6214a9d8e0abc784c34a8598d886b55", null ],
+    [ "ck_tokenizer_mempool_alloc_aligned", "memory__pool_8c.html#a63d36091faee64dbc51d665974939724", null ],
     [ "ck_tokenizer_mempool_available", "memory__pool_8c.html#a8235172a28dfe58996c66c23d36bc8cb", null ],
     [ "ck_tokenizer_mempool_free", "memory__pool_8c.html#a8825ca1a45f75b31e9a24a25856eca88", null ],
     [ "ck_tokenizer_mempool_init", "memory__pool_8c.html#a77ee8ee512daf30f49d730096d202ac5", null ],
     [ "ck_tokenizer_mempool_reset", "memory__pool_8c.html#a27649dad355b3debdf76b54aa983c58f", null ],
-    [ "ck_tokenizer_mempool_strdup", "memory__pool_8c.html#a08371ebc6b399af6459b9f397d584498", null ],
-    [ "ck_tokenizer_mempool_strndup", "memory__pool_8c.html#a04860d1d28aa36dd1e09f4c1fcdfe025", null ],
+    [ "ck_tokenizer_mempool_strdup", "memory__pool_8c.html#a3ada5e9162b4d7d20c219e65ce93b2ac", null ],
+    [ "ck_tokenizer_mempool_strndup", "memory__pool_8c.html#a87b6e97cc5c3ae2145464a7f058b507d", null ],
     [ "ck_tokenizer_mempool_used", "memory__pool_8c.html#a85478de26879bfe01081c3f24b3f43bf", null ]
 ];
