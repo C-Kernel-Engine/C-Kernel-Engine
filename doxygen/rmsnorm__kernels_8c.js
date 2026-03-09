b4a57dc999c16d2fff2227d7aeca06d201387a9e
antshiv <antshiv@users.noreply.github.com>
2026-03-09 22:38:54 +0000

    deploy: 02c0ccea5e2750db50b71562a324dc170c050e4b

--- doxygen/rmsnorm__kernels_8c.js
+++ doxygen/rmsnorm__kernels_8c.js
@@ -1,7 +1,5 @@
 var rmsnorm__kernels_8c =
 [
     [ "rmsnorm_backward", "rmsnorm__kernels_8c.html#aca68d7dda1a7de9ff926920804751db0", null ],
-    [ "rmsnorm_backward_strict_scalar", "rmsnorm__kernels_8c.html#ac429025594088a6caf98de6d42095692", null ],
-    [ "rmsnorm_forward", "rmsnorm__kernels_8c.html#a9a068ed02d809a81a23f09e61b820baf", null ],
-    [ "rmsnorm_forward_strict_scalar", "rmsnorm__kernels_8c.html#a97d8edb56eb75acbad7836dad6cfdbcf", null ]
+    [ "rmsnorm_forward", "rmsnorm__kernels_8c.html#a9a068ed02d809a81a23f09e61b820baf", null ]
 ];
