dacc3664fd049e3a15a6352baa85e2b7b828590a
antshiv <antshiv@users.noreply.github.com>
2026-03-10 22:15:42 +0000

    deploy: 67c3408f293c9c172c6c845ebc64fcdfb41db973

--- doxygen/search/defines_7.js
+++ doxygen/search/defines_7.js
@@ -1,13 +1,15 @@
 var searchData=
 [
-  ['gemm_5fbackend_0',['GEMM_BACKEND',['../gemm__microkernel_8c.html#a1eaf653ddeb5638bd73838e59f35f065',1,'gemm_microkernel.c']]],
-  ['ggml_5ffp16_5fto_5ffp32_1',['ggml_fp16_to_fp32',['../ckernel__quant_8h.html#a15b32ff704be7dc7da90abdc72487a97',1,'GGML_FP16_TO_FP32:&#160;ckernel_quant.h'],['../ckernel__quant_8h.html#abc9374932a8d3d97cd60a4c47b5479ce',1,'ggml_fp16_to_fp32:&#160;ckernel_quant.h']]],
-  ['ggml_5ffp32_5fto_5ffp16_2',['ggml_fp32_to_fp16',['../ckernel__quant_8h.html#a9a15db46a2288701503ab6df9477f8af',1,'GGML_FP32_TO_FP16:&#160;ckernel_quant.h'],['../ckernel__quant_8h.html#a428d91eb599be350065e91e178defd8d',1,'ggml_fp32_to_fp16:&#160;ckernel_quant.h']]],
-  ['gguf_5ftoken_5fbyte_3',['GGUF_TOKEN_BYTE',['../tokenizer__spm_8c.html#ad622d978ef0bbec95bbfd2fb40e031a8',1,'tokenizer_spm.c']]],
-  ['gguf_5ftoken_5fcontrol_4',['GGUF_TOKEN_CONTROL',['../tokenizer__spm_8c.html#a32a873c1bb8103804ea65cfc93fce4b8',1,'tokenizer_spm.c']]],
-  ['gguf_5ftoken_5fnormal_5',['GGUF_TOKEN_NORMAL',['../tokenizer__spm_8c.html#afa4fb7ab0ae07314afc885cfe245962d',1,'tokenizer_spm.c']]],
-  ['gguf_5ftoken_5funknown_6',['GGUF_TOKEN_UNKNOWN',['../tokenizer__spm_8c.html#aebfc8e903d40787ae3bae076f38beb96',1,'tokenizer_spm.c']]],
-  ['gguf_5ftoken_5funused_7',['GGUF_TOKEN_UNUSED',['../tokenizer__spm_8c.html#ab88434b0ebe5c3ab2643fe6074064c31',1,'tokenizer_spm.c']]],
-  ['gguf_5ftoken_5fuser_5fdefined_8',['GGUF_TOKEN_USER_DEFINED',['../tokenizer__spm_8c.html#a80742b73116479d68d521463c2557047',1,'tokenizer_spm.c']]],
-  ['green_9',['GREEN',['../show__config_8c.html#acfbc006ea433ad708fdee3e82996e721',1,'show_config.c']]]
+  ['gemm_5fbackend_4811',['GEMM_BACKEND',['../gemm__microkernel_8c.html#a1eaf653ddeb5638bd73838e59f35f065',1,'gemm_microkernel.c']]],
+  ['ggml_5ffp16_5fto_5ffp32_4812',['ggml_fp16_to_fp32',['../ckernel__quant_8h.html#abc9374932a8d3d97cd60a4c47b5479ce',1,'ckernel_quant.h']]],
+  ['ggml_5ffp16_5fto_5ffp32_4813',['GGML_FP16_TO_FP32',['../ckernel__quant_8h.html#a15b32ff704be7dc7da90abdc72487a97',1,'ckernel_quant.h']]],
+  ['ggml_5ffp32_5fto_5ffp16_4814',['ggml_fp32_to_fp16',['../ckernel__quant_8h.html#a428d91eb599be350065e91e178defd8d',1,'ckernel_quant.h']]],
+  ['ggml_5ffp32_5fto_5ffp16_4815',['GGML_FP32_TO_FP16',['../ckernel__quant_8h.html#a9a15db46a2288701503ab6df9477f8af',1,'ckernel_quant.h']]],
+  ['gguf_5ftoken_5fbyte_4816',['GGUF_TOKEN_BYTE',['../tokenizer_8c.html#ad622d978ef0bbec95bbfd2fb40e031a8',1,'tokenizer.c']]],
+  ['gguf_5ftoken_5fcontrol_4817',['GGUF_TOKEN_CONTROL',['../tokenizer_8c.html#a32a873c1bb8103804ea65cfc93fce4b8',1,'tokenizer.c']]],
+  ['gguf_5ftoken_5fnormal_4818',['GGUF_TOKEN_NORMAL',['../tokenizer_8c.html#afa4fb7ab0ae07314afc885cfe245962d',1,'tokenizer.c']]],
+  ['gguf_5ftoken_5funknown_4819',['GGUF_TOKEN_UNKNOWN',['../tokenizer_8c.html#aebfc8e903d40787ae3bae076f38beb96',1,'tokenizer.c']]],
+  ['gguf_5ftoken_5funused_4820',['GGUF_TOKEN_UNUSED',['../tokenizer_8c.html#ab88434b0ebe5c3ab2643fe6074064c31',1,'tokenizer.c']]],
+  ['gguf_5ftoken_5fuser_5fdefined_4821',['GGUF_TOKEN_USER_DEFINED',['../tokenizer_8c.html#a80742b73116479d68d521463c2557047',1,'tokenizer.c']]],
+  ['green_4822',['GREEN',['../show__config_8c.html#acfbc006ea433ad708fdee3e82996e721',1,'show_config.c']]]
 ];
