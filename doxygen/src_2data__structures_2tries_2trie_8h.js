182f3e8df8e97b99f884cee53804dfcd6a03d4a3
antshiv <antshiv@users.noreply.github.com>
2026-03-11 23:33:56 +0000

    deploy: 8bc1c08a2bf89ae50a2f024f73943a8bb8789a62

--- doxygen/src_2data__structures_2tries_2trie_8h.js
+++ doxygen/src_2data__structures_2tries_2trie_8h.js
@@ -3,7 +3,7 @@ var src_2data__structures_2tries_2trie_8h =
     [ "CKTrie", "structCKTrie.html", "structCKTrie" ],
     [ "CKTrieNode", "structCKTrieNode.html", "structCKTrieNode" ],
     [ "ck_trie_clear", "src_2data__structures_2tries_2trie_8h.html#a245a64683ba5bb40cdffa0c4a6de6980", null ],
-    [ "ck_trie_create", "src_2data__structures_2tries_2trie_8h.html#a142d7df0376883bfddd8c48f008ecdcf", null ],
+    [ "ck_trie_create", "src_2data__structures_2tries_2trie_8h.html#ae53d1f895db72f9ef152b97ddfe032de", null ],
     [ "ck_trie_find_longest", "src_2data__structures_2tries_2trie_8h.html#a77957858c36d4cf9efea8049948f7df3", null ],
     [ "ck_trie_free", "src_2data__structures_2tries_2trie_8h.html#ab747873883085231decec598e26d69ff", null ],
     [ "ck_trie_has_prefix", "src_2data__structures_2tries_2trie_8h.html#aac71332e656ae70059359842c870230e", null ],
