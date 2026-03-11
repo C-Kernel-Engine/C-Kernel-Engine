d80773f06474269f428283b6386117bb4bdb20fe
antshiv <antshiv@users.noreply.github.com>
2026-03-11 23:13:09 +0000

    deploy: 8bc1c08a2bf89ae50a2f024f73943a8bb8789a62

--- doxygen/deltanet__kernels_8c.js
+++ doxygen/deltanet__kernels_8c.js
@@ -2,7 +2,10 @@ var deltanet__kernels_8c =
 [
     [ "CK_DELTANET_MAX_STACK_DIM", "deltanet__kernels_8c.html#ab4de31192bb2a9b88daba6b8693e9483", null ],
     [ "ck_deltanet_l2_inv_norm_scalar", "deltanet__kernels_8c.html#a7652a812c050d10bc20393354d93107f", null ],
+    [ "ck_deltanet_scaled_l2_norm_backward_ref", "deltanet__kernels_8c.html#a09e8db49e7f0b62bb9211d38f5a732a9", null ],
     [ "ck_deltanet_sigmoidf", "deltanet__kernels_8c.html#a9331ad59b9c6268a1f1d4351db2dca6b", null ],
+    [ "gated_deltanet_autoregressive_backward", "deltanet__kernels_8c.html#a13c4e09c2b0eaea0f19c8b162bb9c377", null ],
+    [ "gated_deltanet_autoregressive_backward_ref", "deltanet__kernels_8c.html#abcc51da16224e11073d3716c4f18b708", null ],
     [ "gated_deltanet_autoregressive_forward", "deltanet__kernels_8c.html#ab058f3491df124f04213732b3ae76fce", null ],
     [ "gated_deltanet_autoregressive_forward_ref", "deltanet__kernels_8c.html#a357c0e1f9059fc199d845a50214ba0ac", null ],
     [ "gated_deltanet_impl_name", "deltanet__kernels_8c.html#adc72112b97155ad7cd2360ab95661186", null ]
--- doxygen/navtreeindex19.js
+++ doxygen/navtreeindex19.js
@@ -1,5 +1,9 @@
 var NAVTREEINDEX19 =
 {
+"v6__simple_8c_source.html":[2,0,1,5,7],
+"vision__kernels_8c.html":[2,0,1,1,73],
+"vision__kernels_8c.html#a3ffa58aab7da5c6e77096ad19385329a":[2,0,1,1,73,1],
+"vision__kernels_8c.html#a86aec042bcceb0bd8955f0daf0d986c4":[2,0,1,1,73,0],
 "vision__kernels_8c_source.html":[2,0,1,1,73],
 "vision__kernels__bf16_8c.html":[2,0,1,1,74],
 "vision__kernels__bf16_8c.html#a57139580e075be2d5a65cfffc8e84898":[2,0,1,1,74,0],
