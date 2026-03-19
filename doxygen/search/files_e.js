c8c075425a3632e30fda0f42584fd1ae529daf0a
antshiv <antshiv@users.noreply.github.com>
2026-03-19 05:20:59 +0000

    deploy: 5e8b6d5af8aaef0e022166508d4ca3dfab0f09fb

--- doxygen/search/files_e.js
+++ doxygen/search/files_e.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['qk_5fnorm_5fkernels_2ec_0',['qk_norm_kernels.c',['../qk__norm__kernels_8c.html',1,'']]],
-  ['quantize_5frow_5fq8_5fk_5favx_2ec_1',['quantize_row_q8_k_avx.c',['../quantize__row__q8__k__avx_8c.html',1,'']]],
-  ['quantize_5frow_5fq8_5fk_5favx2_2ec_2',['quantize_row_q8_k_avx2.c',['../quantize__row__q8__k__avx2_8c.html',1,'']]],
-  ['quantize_5frow_5fq8_5fk_5fsse_2ec_3',['quantize_row_q8_k_sse.c',['../quantize__row__q8__k__sse_8c.html',1,'']]]
+  ['qk_5fnorm_5fkernels_2ec_2673',['qk_norm_kernels.c',['../qk__norm__kernels_8c.html',1,'']]],
+  ['quantize_5frow_5fq8_5fk_5fsse_2ec_2674',['quantize_row_q8_k_sse.c',['../quantize__row__q8__k__sse_8c.html',1,'']]]
 ];
