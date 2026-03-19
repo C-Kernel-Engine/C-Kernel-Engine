8f6c77eeb87526501d0590cbdcb924b0ebbde43a
antshiv <antshiv@users.noreply.github.com>
2026-03-19 23:55:49 +0000

    deploy: ce77b8de170eeefb83e218b240031400e1e4e328

--- doxygen/search/files_3.js
+++ doxygen/search/files_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['dequant_5fkernels_2ec_2611',['dequant_kernels.c',['../dequant__kernels_8c.html',1,'']]]
+  ['deltanet_5fkernels_2ec_0',['deltanet_kernels.c',['../deltanet__kernels_8c.html',1,'']]],
+  ['dequant_5fkernels_2ec_1',['dequant_kernels.c',['../dequant__kernels_8c.html',1,'']]]
 ];
