f4ff31eda9ab5cb6edf564e058e7ec454e12d8c8
antshiv <antshiv@users.noreply.github.com>
2026-03-22 11:43:50 +0000

    deploy: 02daf8631dcde75c77a4e0d0e90b8f7a61acdc66

--- doxygen/ckernel__alloc_8c.js
+++ doxygen/ckernel__alloc_8c.js
@@ -3,9 +3,9 @@ var ckernel__alloc_8c =
     [ "_GNU_SOURCE", "ckernel__alloc_8c.html#a369266c24eacffb87046522897a570d5", null ],
     [ "HUGE_PAGE_SIZE", "ckernel__alloc_8c.html#af40ff2d14fdebba00915237c11b490c8", null ],
     [ "align_up_bytes", "ckernel__alloc_8c.html#a19108c1ddea653d46840daefcf542bb5", null ],
-    [ "ck_huge_alloc", "ckernel__alloc_8c.html#a11a682ae32301dd05465e77d7dd4e37e", null ],
+    [ "ck_huge_alloc", "ckernel__alloc_8c.html#ada1defb15c658adb17b9058087525057", null ],
     [ "ck_huge_free", "ckernel__alloc_8c.html#af814a916b578d2824aa0e9ec230ea166", null ],
-    [ "detach_allocation", "ckernel__alloc_8c.html#a1e5036eee15a005216b507a534db66b6", null ],
+    [ "detach_allocation", "ckernel__alloc_8c.html#a9483fdd187f4e96dce86fdfa0bc060fe", null ],
     [ "record_allocation", "ckernel__alloc_8c.html#a78d026646faf03ea83ff458d93f99b6a", null ],
     [ "g_alloc_list", "ckernel__alloc_8c.html#a34793e13c1b596328dfcc805c14381ab", null ],
     [ "g_alloc_mutex", "ckernel__alloc_8c.html#ae8cf2951c2be25ccbe20a7bf9f9ff32b", null ]
--- doxygen/search/files_7.js
+++ doxygen/search/files_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['hash_5ftable_2ec_2647',['hash_table.c',['../hash__table_8c.html',1,'']]],
-  ['hash_5ftable_2eh_2648',['hash_table.h',['../hash__table_8h.html',1,'']]]
+  ['hash_5ftable_2ec_0',['hash_table.c',['../hash__table_8c.html',1,'']]],
+  ['hash_5ftable_2eh_1',['hash_table.h',['../hash__table_8h.html',1,'']]],
+  ['hybrid_5fattention_5fkernels_2ec_2',['hybrid_attention_kernels.c',['../hybrid__attention__kernels_8c.html',1,'']]]
 ];
