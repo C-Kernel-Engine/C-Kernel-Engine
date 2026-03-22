20e71ed23da3b84be6baa4c430323e7a89c8e1c3
antshiv <antshiv@users.noreply.github.com>
2026-03-22 12:05:29 +0000

    deploy: 02daf8631dcde75c77a4e0d0e90b8f7a61acdc66

--- doxygen/search/searchdata.js
+++ doxygen/search/searchdata.js
@@ -1,15 +1,15 @@
 var indexSectionsWithContent =
 {
-  0: "026_abcdefghijklmnopqrstuvwyz",
+  0: "_abcdefghijklmnopqrstuvwyz",
   1: "abcgmnpqrst",
   2: "abcdefghiklmopqrstuv",
   3: "_abcdefghijklmopqrstuvwz",
   4: "abcdefghiklmnopqrstuvw",
   5: "cdefgiksv",
-  6: "cqr",
-  7: "cqr",
+  6: "cr",
+  7: "cr",
   8: "_abcdefghikmnopqrswy",
-  9: "lt"
+  9: "t"
 };
 
 var indexSectionNames =
