6ff6eb9f49584a8aeae77510abbcfc93a0ec15af
antshiv <antshiv@users.noreply.github.com>
2026-03-28 01:34:26 +0000

    deploy: 0ca399a796ff09a2f5cd970d69b3e0223dd6d15d

--- doxygen/loss__kernels_8c.js
+++ doxygen/loss__kernels_8c.js
@@ -1,4 +1,11 @@
 var loss__kernels_8c =
 [
-    [ "softmax_cross_entropy_loss", "loss__kernels_8c.html#aed0168fe0364ab17b0d9dd02b1ac3ffa", null ]
+    [ "ce_legacy_mode_enabled", "loss__kernels_8c.html#a2411094198bfa4c18e0aae09fcb8bd90", null ],
+    [ "ce_targets_all_valid_no_ignore", "loss__kernels_8c.html#a132bd99a38413d1f66907b26521e26ea", null ],
+    [ "parse_env_bool_on", "loss__kernels_8c.html#aa4c866ab949806419445fee35b401f90", null ],
+    [ "softmax_cross_entropy_loss", "loss__kernels_8c.html#aed0168fe0364ab17b0d9dd02b1ac3ffa", null ],
+    [ "softmax_cross_entropy_loss_index_mean_impl", "loss__kernels_8c.html#a5ab310b0c0637c7a5bb050fc7d3c52e5", null ],
+    [ "softmax_cross_entropy_loss_legacy_mean_impl", "loss__kernels_8c.html#a5d86d680b048f5cb1bfe3d7ceecd6100", null ],
+    [ "softmax_cross_entropy_loss_ptref", "loss__kernels_8c.html#a06de4a9f510b34ead5fdf863738b4448", null ],
+    [ "zero_row_f32", "loss__kernels_8c.html#a50742b26555eda12a7cd3331aa69ffe7", null ]
 ];
--- doxygen/recurrent__split__kernels_8c.js
+++ doxygen/recurrent__split__kernels_8c.js
@@ -3,5 +3,6 @@ var recurrent__split__kernels_8c =
     [ "recurrent_split_conv_qkv_backward", "recurrent__split__kernels_8c.html#a147c4ce1106e978ae62cdc54eff7002d", null ],
     [ "recurrent_split_conv_qkv_forward", "recurrent__split__kernels_8c.html#a53ca38835096b418b4a2089db9a24b40", null ],
     [ "recurrent_split_qkv_backward", "recurrent__split__kernels_8c.html#a3f02cdb7838789a010d2fb34b94f2a61", null ],
-    [ "recurrent_split_qkv_forward", "recurrent__split__kernels_8c.html#adc53b91db62ed02dfdfee0b9d086b9e2", null ]
+    [ "recurrent_split_qkv_forward", "recurrent__split__kernels_8c.html#adc53b91db62ed02dfdfee0b9d086b9e2", null ],
+    [ "split_qkv_packed_head_major_forward", "recurrent__split__kernels_8c.html#a9525efb4fcb62c7c88e7f4bc369ca1b8", null ]
 ];
