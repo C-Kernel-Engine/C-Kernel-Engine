6e9d0b390798819e9401aa96be6685305a24a4fb
antshiv <antshiv@users.noreply.github.com>
2026-04-08 19:52:13 +0000

    deploy: 4552bc588440ad2060fad79129e04eb14784c421

--- doxygen/search/enumvalues_1.js
+++ doxygen/search/enumvalues_1.js
@@ -1,11 +1,12 @@
 var searchData=
 [
-  ['ggml_5fstatus_5faborted_0',['GGML_STATUS_ABORTED',['../ggml__runtime__compat_8h.html#a25b8976b70962cc9b52c3508eda235adaa6be8964844007105224d4cafc3ff7c9',1,'ggml_runtime_compat.h']]],
-  ['ggml_5fstatus_5falloc_5ffailed_1',['GGML_STATUS_ALLOC_FAILED',['../ggml__runtime__compat_8h.html#a25b8976b70962cc9b52c3508eda235ada80fe0445773a0394a2792a9726971121',1,'ggml_runtime_compat.h']]],
-  ['ggml_5fstatus_5ffailed_2',['GGML_STATUS_FAILED',['../ggml__runtime__compat_8h.html#a25b8976b70962cc9b52c3508eda235adad304e0b5bcd4b5533a6ccb3c303e6510',1,'ggml_runtime_compat.h']]],
-  ['ggml_5fstatus_5fsuccess_3',['GGML_STATUS_SUCCESS',['../ggml__runtime__compat_8h.html#a25b8976b70962cc9b52c3508eda235ada2ca89f294003a9d508d32dd79087223c',1,'ggml_runtime_compat.h']]],
-  ['ggml_5ftype_5ff16_4',['GGML_TYPE_F16',['../ggml__runtime__compat_8h.html#a7f5db4faa296c99fcdd51cb4154df0ffa816bc3c05f2d373ea1020a5129b4c45e',1,'ggml_runtime_compat.h']]],
-  ['ggml_5ftype_5ff32_5',['GGML_TYPE_F32',['../ggml__runtime__compat_8h.html#a7f5db4faa296c99fcdd51cb4154df0ffa43fc3f0592a787222046fb4cf2605831',1,'ggml_runtime_compat.h']]],
-  ['ggml_5ftype_5fi32_6',['GGML_TYPE_I32',['../ggml__runtime__compat_8h.html#a7f5db4faa296c99fcdd51cb4154df0ffaceae923c5cf135cd4ee2c34d610c735d',1,'ggml_runtime_compat.h']]],
-  ['ggml_5ftype_5fq8_5f0_7',['GGML_TYPE_Q8_0',['../ggml__runtime__compat_8h.html#a7f5db4faa296c99fcdd51cb4154df0ffaca429d8163b59c13f4b274b1fbe483bc',1,'ggml_runtime_compat.h']]]
+  ['rec_5fcategory_5faffinity_4675',['REC_CATEGORY_AFFINITY',['../system__topology_8h.html#abc3893de9bbd40dc5a7191b3dedcf446a2c69bb37dc1534af0986f7fb4a63f83b',1,'system_topology.h']]],
+  ['rec_5fcategory_5fcpu_4676',['REC_CATEGORY_CPU',['../system__topology_8h.html#abc3893de9bbd40dc5a7191b3dedcf446a532135b3b6ed583aaf4bf2864793255e',1,'system_topology.h']]],
+  ['rec_5fcategory_5fmemory_4677',['REC_CATEGORY_MEMORY',['../system__topology_8h.html#abc3893de9bbd40dc5a7191b3dedcf446a86e1aa77256345474354d36dade4c2cc',1,'system_topology.h']]],
+  ['rec_5fcategory_5fnetwork_4678',['REC_CATEGORY_NETWORK',['../system__topology_8h.html#abc3893de9bbd40dc5a7191b3dedcf446aa27b104e8216e409c0e049077a3259ba',1,'system_topology.h']]],
+  ['rec_5fcategory_5fpcie_4679',['REC_CATEGORY_PCIE',['../system__topology_8h.html#abc3893de9bbd40dc5a7191b3dedcf446ab4c62d868d6055729709c1e2b51bf688',1,'system_topology.h']]],
+  ['rec_5fpriority_5fcritical_4680',['REC_PRIORITY_CRITICAL',['../system__topology_8h.html#aabe7a1b1ca3c4e61061d2fbc43762a69ad2c44c068f9a309b584934d165738405',1,'system_topology.h']]],
+  ['rec_5fpriority_5fhigh_4681',['REC_PRIORITY_HIGH',['../system__topology_8h.html#aabe7a1b1ca3c4e61061d2fbc43762a69ae2b3a4412e439de7cd932d5c9c239628',1,'system_topology.h']]],
+  ['rec_5fpriority_5flow_4682',['REC_PRIORITY_LOW',['../system__topology_8h.html#aabe7a1b1ca3c4e61061d2fbc43762a69aff18fa78a746a2cfe6521f96f2985d0d',1,'system_topology.h']]],
+  ['rec_5fpriority_5fmedium_4683',['REC_PRIORITY_MEDIUM',['../system__topology_8h.html#aabe7a1b1ca3c4e61061d2fbc43762a69ac060aa16ba5ff6de278ec1a69c992740',1,'system_topology.h']]]
 ];
