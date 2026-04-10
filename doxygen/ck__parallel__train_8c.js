8a0e1b5ba6c13c16cc7102a3f89f7a1a7707d478
antshiv <antshiv@users.noreply.github.com>
2026-04-10 12:36:33 +0000

    deploy: c481288958c4bdb6e5cda7d4d8034e395f9dd49a

--- doxygen/ck__parallel__train_8c.js
+++ doxygen/ck__parallel__train_8c.js
@@ -1,10 +1,16 @@
 var ck__parallel__train_8c =
 [
-    [ "ck_train_bias_reduce_work", "ck__parallel__train_8c.html#ac194e5f15a1a378fbe08fbeb7fe51bda", null ],
+    [ "ck_train_bias_reduce_compute_range", "ck__parallel__train_8c.html#a074f3ba4e2e5e250a1d3da3ec881a25c", null ],
+    [ "ck_train_gemm_backward_serial", "ck__parallel__train_8c.html#a2c28131807e759d4933c69143c37c0ba", null ],
+    [ "ck_train_gemm_backward_work", "ck__parallel__train_8c.html#ac3fecc2babce4b360bf97501453fd028", null ],
+    [ "ck_train_gemm_nn_compute_rows", "ck__parallel__train_8c.html#aa8229740abfe4a2485f907d5f18aa0b5", null ],
     [ "ck_train_gemm_nn_work", "ck__parallel__train_8c.html#a986e0d9835c5a7b498b2707b54f95c3c", null ],
-    [ "ck_train_gemm_tn_work", "ck__parallel__train_8c.html#ad6a80c3e5aed0b7505405c8f98e38927", null ],
+    [ "ck_train_gemm_nt_compute_rows", "ck__parallel__train_8c.html#a9ee22d24eadc82b1301c846eb733c31e", null ],
+    [ "ck_train_gemm_tn_compute_rows", "ck__parallel__train_8c.html#a23c65a37b80688c898f8b23bad231b06", null ],
     [ "ck_train_gemm_work", "ck__parallel__train_8c.html#ac765dc2464c4e0750fedb53482dab5d4", null ],
+    [ "ck_train_outer_t1_compute_range", "ck__parallel__train_8c.html#a2b504075b20a21738b38ab615e401616", null ],
     [ "ck_train_outer_t1_work", "ck__parallel__train_8c.html#ae80e32a8305af752aa5249ae5b35b2a5", null ],
+    [ "ck_train_pick_active_threads", "ck__parallel__train_8c.html#a7ce5262e71f41b17dfaf26498e0caade", null ],
     [ "gemm_backward_f32_train_parallel_dispatch", "ck__parallel__train_8c.html#ae87bd55345149dc971345404a7fc731a", null ],
     [ "gemm_backward_f32_train_parallel_dispatch_v2", "ck__parallel__train_8c.html#a395a6655cf6d15d0fed383075adae4ec", null ],
     [ "gemm_blocked_serial_train_parallel_dispatch", "ck__parallel__train_8c.html#a349c1f58c04593f6589c4f74a3732e0f", null ]
