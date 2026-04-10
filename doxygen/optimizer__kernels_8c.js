17b096167c0518ced7acda2703c3a9869ab673ca
antshiv <antshiv@users.noreply.github.com>
2026-04-10 13:07:41 +0000

    deploy: c481288958c4bdb6e5cda7d4d8034e395f9dd49a

--- doxygen/optimizer__kernels_8c.js
+++ doxygen/optimizer__kernels_8c.js
@@ -1,26 +1,9 @@
 var optimizer__kernels_8c =
 [
-    [ "CK_OPT_PAR_MAX_THREADS", "optimizer__kernels_8c.html#adb28b128f8218c37a63961ddc726db93", null ],
-    [ "CK_OPT_PAR_MIN_NUMEL", "optimizer__kernels_8c.html#a62fd0a2d854973891c609fe9f7654a98", null ],
-    [ "adamw_clip_update_multi_f32", "optimizer__kernels_8c.html#ab5a989d66800118a460f2229f856aa3d", null ],
     [ "adamw_update_f32", "optimizer__kernels_8c.html#ac1a9ed01c15286fc6e3be73f96371d76", null ],
-    [ "adamw_update_f32_impl", "optimizer__kernels_8c.html#a1b5c89658044de1c75abb8822b2ac8a5", null ],
-    [ "ck_accum_multi_parallel_work", "optimizer__kernels_8c.html#a017e1c756e664d84e72880c3afa3dbc2", null ],
-    [ "ck_accum_parallel_work", "optimizer__kernels_8c.html#afe3fc98bce4b7a987ef5625790670202", null ],
-    [ "ck_adamw_multi_parallel_work", "optimizer__kernels_8c.html#aa599f974430c367a3cbe0f34c0899daa", null ],
-    [ "ck_adamw_parallel_work", "optimizer__kernels_8c.html#abde072e262515d74d1e14d7a4c2562c2", null ],
-    [ "ck_opt_pick_active_threads", "optimizer__kernels_8c.html#a5245cacc9e33f88db872de213e887c5a", null ],
-    [ "ck_scale_parallel_work", "optimizer__kernels_8c.html#a47011358c2861776076ba39710b34455", null ],
-    [ "ck_sum_sq_multi_parallel_work", "optimizer__kernels_8c.html#a13410fec4c5089df3d3fc859d1a44c16", null ],
-    [ "ck_sum_sq_parallel_work", "optimizer__kernels_8c.html#a779237f70517f42f8405f4548c51d301", null ],
     [ "gradient_accumulate_f32", "optimizer__kernels_8c.html#ac43e9094fbc4956d450b5f6deb5ae899", null ],
-    [ "gradient_accumulate_f32_impl", "optimizer__kernels_8c.html#aee22d3d7aa1487e535d06671dd921d8d", null ],
-    [ "gradient_accumulate_multi_f32", "optimizer__kernels_8c.html#a696dd0104d72f1bd5c6ddc6fe884b29a", null ],
     [ "gradient_clip_norm_f32", "optimizer__kernels_8c.html#a4e854ffc919637e40ea0d320ecb5ed0b", null ],
-    [ "gradient_global_norm_multi_f32", "optimizer__kernels_8c.html#aa015a15200e971e831d802f787456e2c", null ],
     [ "gradient_scale_f32", "optimizer__kernels_8c.html#ab615ca5560d68ed2f098960cd81acfc2", null ],
-    [ "gradient_scale_f32_impl", "optimizer__kernels_8c.html#ab32c3fa8d265f54261c4ce219313aa9a", null ],
-    [ "gradient_sum_sq_f32_impl", "optimizer__kernels_8c.html#a440ffb33cc72ca105d69150362181030", null ],
     [ "sgd_momentum_update_f32", "optimizer__kernels_8c.html#a1c1d97635474696da41a9f318ac7600b", null ],
     [ "zero_gradients_f32", "optimizer__kernels_8c.html#adecdd5f34f2c7634e7e665ab3d96da37", null ]
 ];
