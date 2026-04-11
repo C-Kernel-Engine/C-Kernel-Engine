af36594c5d060a596192b10dfa6bae7d1715c925
antshiv <antshiv@users.noreply.github.com>
2026-04-11 11:30:46 +0000

    deploy: c3f6c096f2136e28fa8a314b096a1e78aa9a221d

--- doxygen/rmsnorm__qkv_8c.js
+++ doxygen/rmsnorm__qkv_8c.js
@@ -1,6 +1,7 @@
 var rmsnorm__qkv_8c =
 [
     [ "compute_rms_scale", "rmsnorm__qkv_8c.html#a2906e6ea2b3e02de749db253bf46d337", null ],
+    [ "rmsnorm_forward", "rmsnorm__qkv_8c.html#a9a068ed02d809a81a23f09e61b820baf", null ],
     [ "rmsnorm_qkv_fp32_fused", "rmsnorm__qkv_8c.html#a8566c7b232739c7dac0a3ea3d5666e99", null ],
     [ "rmsnorm_qkv_fp32_fused_v2", "rmsnorm__qkv_8c.html#a4db47e25b31ee405e47bd65bbfb04d64", null ],
     [ "rmsnorm_qkv_fp32_fused_v3", "rmsnorm__qkv_8c.html#a317ac64c0e72e8220365e58f4e97122a", null ],
