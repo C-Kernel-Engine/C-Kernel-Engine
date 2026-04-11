26b1c740b467ef9e4f53a4fa4133e64abdc1ffaf
antshiv <antshiv@users.noreply.github.com>
2026-04-11 15:44:16 +0000

    deploy: 5342ace453caed49faf79129198c00285738e78d

---
+++ doxygen/structck__bump__alloc__t.js
@@ -0,0 +1,10 @@
+var structck__bump__alloc__t =
+[
+    [ "activations_base", "structck__bump__alloc__t.html#a197ed664fe2b6dd05130e00c6093225d", null ],
+    [ "base", "structck__bump__alloc__t.html#a66772500081c422f01e036ff8f6db361", null ],
+    [ "mapped_len", "structck__bump__alloc__t.html#a64fefe4e274a20faac155fc87b4dfc4a", null ],
+    [ "mode", "structck__bump__alloc__t.html#ac9f12cd3ed6cd33ea08e2bcabe752d9c", null ],
+    [ "total_size", "structck__bump__alloc__t.html#ac6dedb478c486e2b22066b7bef49676d", null ],
+    [ "weights_base", "structck__bump__alloc__t.html#a02f56f6e9492c422502b8d69a9d266cc", null ],
+    [ "weights_file_size", "structck__bump__alloc__t.html#ab1f7de12a27705ebfc6390028d494a4b", null ]
+];
