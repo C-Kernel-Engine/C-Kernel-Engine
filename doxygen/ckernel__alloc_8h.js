ac03e02204787ac34002530262b134da93d11873
antshiv <antshiv@users.noreply.github.com>
2026-04-14 14:44:17 +0000

    deploy: 991d08741be9869ed00bc2ae9e0749fae31ccb58

--- doxygen/ckernel__alloc_8h.js
+++ doxygen/ckernel__alloc_8h.js
@@ -1,5 +1,14 @@
 var ckernel__alloc_8h =
 [
-    [ "ck_huge_alloc", "ckernel__alloc_8h.html#a11a682ae32301dd05465e77d7dd4e37e", null ],
+    [ "ck_bump_alloc_t", "structck__bump__alloc__t.html", "structck__bump__alloc__t" ],
+    [ "ck_bump_mode_t", "ckernel__alloc_8h.html#a0da00fcc3e414b93752ee0dd9d279e37", [
+      [ "CK_BUMP_MODE_UNINITIALIZED", "ckernel__alloc_8h.html#a0da00fcc3e414b93752ee0dd9d279e37ab8e3208f81ccb32730270a4efa84a033", null ],
+      [ "CK_BUMP_MODE_ANON", "ckernel__alloc_8h.html#a0da00fcc3e414b93752ee0dd9d279e37a00e422091cb4a66d33750afcda75068a", null ],
+      [ "CK_BUMP_MODE_MIXED_FILE_BACKED", "ckernel__alloc_8h.html#a0da00fcc3e414b93752ee0dd9d279e37a0b7c2025d4e1c115d94a20dd029cc35c", null ]
+    ] ],
+    [ "ck_bump_alloc_free", "ckernel__alloc_8h.html#a061d0a5618d0d2b70adb0b6df132b30d", null ],
+    [ "ck_bump_alloc_init", "ckernel__alloc_8h.html#a08198388fe890bfe4b28ab6edbb9fea0", null ],
+    [ "ck_bump_alloc_needs_weight_materialization", "ckernel__alloc_8h.html#afdf45ab35654e7a2a7e52a624a4b737b", null ],
+    [ "ck_huge_alloc", "ckernel__alloc_8h.html#ada1defb15c658adb17b9058087525057", null ],
     [ "ck_huge_free", "ckernel__alloc_8h.html#af814a916b578d2824aa0e9ec230ea166", null ]
 ];
