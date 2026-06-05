5738a901bbdbc2a8dae5ecddc3242002a70fb77b
antshiv <antshiv@users.noreply.github.com>
2026-06-05 21:13:39 +0000

    deploy: a126b6226883fbbb4e7b8c8b5f40713323e89f50

--- doxygen/embedding__kernels__bf16_8c.js
+++ doxygen/embedding__kernels__bf16_8c.js
@@ -1,6 +1,5 @@
 var embedding__kernels__bf16_8c =
 [
     [ "embedding_backward_bf16", "embedding__kernels__bf16_8c.html#a7c57b105fb88eb4165a0c649932be08d", null ],
-    [ "embedding_backward_bf16_mixed", "embedding__kernels__bf16_8c.html#adc97d3c00f6b59b24d8680e142ecc309", null ],
     [ "embedding_forward_bf16", "embedding__kernels__bf16_8c.html#a6d58689c662e670a5423eedd0291d6b5", null ]
 ];
