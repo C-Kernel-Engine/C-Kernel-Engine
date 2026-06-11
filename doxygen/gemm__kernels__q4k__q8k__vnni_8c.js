0dff009f3aa452c214b58645d0554e7cf8dcb33c
antshiv <antshiv@users.noreply.github.com>
2026-06-11 02:25:53 +0000

    deploy: 221f0ae813c35026432fe4d67b9119ac503d62ec

--- doxygen/gemm__kernels__q4k__q8k__vnni_8c.js
+++ doxygen/gemm__kernels__q4k__q8k__vnni_8c.js
@@ -1,6 +1,7 @@
 var gemm__kernels__q4k__q8k__vnni_8c =
 [
     [ "gemv_q4_k_q8_k_avx2", "gemm__kernels__q4k__q8k__vnni_8c.html#a030c98b26ad610ccea6dabf993b60cdf", null ],
+    [ "gemv_q4_k_q8_k_parallel_vnni", "gemm__kernels__q4k__q8k__vnni_8c.html#a0ea9f437d0af65398100c4c50c85ece6", null ],
     [ "gemv_q4_k_q8_k_ref", "gemm__kernels__q4k__q8k__vnni_8c.html#ac01f8caf84e100f3c72919c71217010e", null ],
     [ "gemv_q4_k_q8_k_vnni", "gemm__kernels__q4k__q8k__vnni_8c.html#a53c1355c32a77df5522e2b8eff8fc3f5", null ]
 ];
