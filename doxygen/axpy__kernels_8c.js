3616ac0156dc07d27bb79e9765727d36d4435e73
antshiv <antshiv@users.noreply.github.com>
2026-06-20 03:31:11 +0000

    deploy: c2fcae451b430f231fb3a3cc87b26a5ef994a8a8

--- doxygen/axpy__kernels_8c.js
+++ doxygen/axpy__kernels_8c.js
@@ -3,11 +3,7 @@ var axpy__kernels_8c =
     [ "axpy_2d_f32", "axpy__kernels_8c.html#a8b41caed90aece96a82840ac7d43206d", null ],
     [ "axpy_f32", "axpy__kernels_8c.html#a05c2cabc2f651bd23803aa46f953586e", null ],
     [ "axpy_zero_f32", "axpy__kernels_8c.html#a30a0783466daf24cb1bc870cc0bf37f0", null ],
-    [ "ck_moe_down_idx", "axpy__kernels_8c.html#a288eaa3afb8eca1b77bf90ecbf5fed80", null ],
-    [ "ck_moe_up_idx", "axpy__kernels_8c.html#a1dbe012150b76ea78303e526a35f93fc", null ],
     [ "moe_accumulate_expert_f32", "axpy__kernels_8c.html#a0df9f0994c879eaa00c6ba9c0867bbec", null ],
-    [ "moe_relu2_expert_backward_f32", "axpy__kernels_8c.html#a3d0ee5eba40a47c33dc5c9c6cd4453c0", null ],
-    [ "moe_relu2_expert_forward_f32", "axpy__kernels_8c.html#a61a81e4029a3cc37bea14e7ee7017979", null ],
     [ "scal_copy_f32", "axpy__kernels_8c.html#af46703c3da9c9763447aee49e55a4cd7", null ],
     [ "weighted_sum_f32", "axpy__kernels_8c.html#a6923864d9edb216c4938436cd74a4823", null ]
 ];
