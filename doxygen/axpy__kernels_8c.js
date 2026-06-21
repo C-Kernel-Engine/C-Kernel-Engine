674b1f8b0f67fe45e725983b5d3f1a81bf159247
antshiv <antshiv@users.noreply.github.com>
2026-06-21 14:00:34 +0000

    deploy: d3f5d6be97521ba5f0757bfd58408579614742cf

--- doxygen/axpy__kernels_8c.js
+++ doxygen/axpy__kernels_8c.js
@@ -3,16 +3,7 @@ var axpy__kernels_8c =
     [ "axpy_2d_f32", "axpy__kernels_8c.html#a8b41caed90aece96a82840ac7d43206d", null ],
     [ "axpy_f32", "axpy__kernels_8c.html#a05c2cabc2f651bd23803aa46f953586e", null ],
     [ "axpy_zero_f32", "axpy__kernels_8c.html#a30a0783466daf24cb1bc870cc0bf37f0", null ],
-    [ "ck_moe_debug_enabled", "axpy__kernels_8c.html#a1dacada9ab42215f00fdd77696883afb", null ],
-    [ "ck_moe_debug_finite", "axpy__kernels_8c.html#ab9841ab8e2a01e02d7200e780fdca643", null ],
-    [ "ck_moe_down_idx", "axpy__kernels_8c.html#a288eaa3afb8eca1b77bf90ecbf5fed80", null ],
-    [ "ck_moe_up_idx", "axpy__kernels_8c.html#a1dbe012150b76ea78303e526a35f93fc", null ],
     [ "moe_accumulate_expert_f32", "axpy__kernels_8c.html#a0df9f0994c879eaa00c6ba9c0867bbec", null ],
-    [ "moe_relu2_expert_backward_f32", "axpy__kernels_8c.html#a3d0ee5eba40a47c33dc5c9c6cd4453c0", null ],
-    [ "moe_relu2_expert_forward_f32", "axpy__kernels_8c.html#a61a81e4029a3cc37bea14e7ee7017979", null ],
-    [ "moe_relu2_expert_forward_q5_0_q5_0", "axpy__kernels_8c.html#a930950189b33a15158c85363e41a3410", null ],
-    [ "moe_relu2_expert_forward_q5_0_q8_0", "axpy__kernels_8c.html#a242ef973cca255ef35c5809f737f66fb", null ],
-    [ "moe_relu2_shared_forward_q5_1_q8_0", "axpy__kernels_8c.html#a8f6e62274ae108c8dffe6921048b7758", null ],
     [ "scal_copy_f32", "axpy__kernels_8c.html#af46703c3da9c9763447aee49e55a4cd7", null ],
     [ "weighted_sum_f32", "axpy__kernels_8c.html#a6923864d9edb216c4938436cd74a4823", null ]
 ];
