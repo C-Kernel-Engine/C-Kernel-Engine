da675120d6f58ddb54f5b8773a7fcf6ed0720366
antshiv <antshiv@users.noreply.github.com>
2026-06-22 14:11:09 +0000

    deploy: 820a6a265ba4b18d3d351991e43b627267b5bd6b

--- doxygen/ckernel__ir__v2__builder_8c.js
+++ doxygen/ckernel__ir__v2__builder_8c.js
@@ -7,12 +7,12 @@ var ckernel__ir__v2__builder_8c =
     [ "ck_ir_v2_copy_buffer_spec", "ckernel__ir__v2__builder_8c.html#a29e53bd4efe76f71f9f493bd6ca03364", null ],
     [ "ck_ir_v2_copy_shape", "ckernel__ir__v2__builder_8c.html#a848d051811b6e6f10ed7c7747360fd5b", null ],
     [ "ck_ir_v2_find_buffer_index", "ckernel__ir__v2__builder_8c.html#ad0ca6b1fd812a332aa86eef694f67a8f", null ],
-    [ "ck_ir_v2_find_kernel_spec", "ckernel__ir__v2__builder_8c.html#af49195eecf6a337b1d16a56ed0e37922", null ],
-    [ "ck_ir_v2_find_key", "ckernel__ir__v2__builder_8c.html#a8be1b56b21de19f8cde1e7bfe390ffad", null ],
+    [ "ck_ir_v2_find_kernel_spec", "ckernel__ir__v2__builder_8c.html#a19d5b6fb75d3ff9f5977d592d801c58f", null ],
+    [ "ck_ir_v2_find_key", "ckernel__ir__v2__builder_8c.html#a0bd07117271feea83db8668445f3c9ca", null ],
     [ "ck_ir_v2_parse_bool", "ckernel__ir__v2__builder_8c.html#a39594713511ebb496afee8c67c18c60b", null ],
     [ "ck_ir_v2_parse_dtype", "ckernel__ir__v2__builder_8c.html#acf24a677d11c08305e6c3f383baeabb2", null ],
     [ "ck_ir_v2_parse_string", "ckernel__ir__v2__builder_8c.html#a7aa30708242122818555e5bb33b47f99", null ],
-    [ "ck_ir_v2_select_kernel", "ckernel__ir__v2__builder_8c.html#a2a289dfc861e40f2cc178fd95b37b0e5", null ],
-    [ "ck_ir_v2_skip_ws", "ckernel__ir__v2__builder_8c.html#a494e3c508c52e2a879d635959142382b", null ],
-    [ "ck_ir_v2_strdup", "ckernel__ir__v2__builder_8c.html#a104bb744263fc943e02e9fb5e66632bf", null ]
+    [ "ck_ir_v2_select_kernel", "ckernel__ir__v2__builder_8c.html#ae3041e9f0dea938a7f37c7c0aecae866", null ],
+    [ "ck_ir_v2_skip_ws", "ckernel__ir__v2__builder_8c.html#a15f22b10dc9651308fe42dcd0807319e", null ],
+    [ "ck_ir_v2_strdup", "ckernel__ir__v2__builder_8c.html#af2f42a1dfff034eb16c334bb5403a977", null ]
 ];
