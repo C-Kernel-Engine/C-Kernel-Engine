b897932ba777c4806bb36e70e6eab939586519a2
antshiv <antshiv@users.noreply.github.com>
2026-07-03 15:18:07 +0000

    deploy: 24cf9dd85c68f157684a8b39963c90fde38bb7b5

--- doxygen/search/functions_17.js
+++ doxygen/search/functions_17.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['zero_5fgradients_5fbf16_3877',['zero_gradients_bf16',['../optimizer__kernels__bf16_8c.html#a0461e04c92bea2dfd26c2f8cff17c871',1,'optimizer_kernels_bf16.c']]],
-  ['zero_5fgradients_5ff32_3878',['zero_gradients_f32',['../optimizer__kernels_8c.html#adecdd5f34f2c7634e7e665ab3d96da37',1,'optimizer_kernels.c']]],
-  ['zero_5flayernorm_5fpadding_3879',['zero_layernorm_padding',['../layernorm__kernels_8c.html#a70f6e1d9794261350ce1a16212a4e0bb',1,'layernorm_kernels.c']]]
+  ['weighted_5fsum_5ff32_0',['weighted_sum_f32',['../ckernel__engine_8h.html#a6923864d9edb216c4938436cd74a4823',1,'weighted_sum_f32(float *y, const float **vectors, const float *weights, int k, int n):&#160;axpy_kernels.c'],['../axpy__kernels_8c.html#a6923864d9edb216c4938436cd74a4823',1,'weighted_sum_f32(float *y, const float **vectors, const float *weights, int k, int n):&#160;axpy_kernels.c']]],
+  ['worker_5fmain_1',['worker_main',['../ck__threadpool_8c.html#ab50c928126740940413626fd7ab7ce72',1,'ck_threadpool.c']]]
 ];
