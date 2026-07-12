7108ce268abc2ee29e9f2247b66ec3e5fd2aafb0
antshiv <antshiv@users.noreply.github.com>
2026-07-12 12:19:31 +0000

    deploy: e4a57d1b27c0897bc337ebace21721170e020b20

--- doxygen/gemm__kernels__bf16_8c.js
+++ doxygen/gemm__kernels__bf16_8c.js
@@ -5,12 +5,8 @@ var gemm__kernels__bf16_8c =
     [ "BLK_N", "gemm__kernels__bf16_8c.html#a486dca13feef2c2026f323b59e1a44df", null ],
     [ "__attribute__", "gemm__kernels__bf16_8c.html#a36f229bbda63818246ec86181b1fdd42", null ],
     [ "ck_min_i", "gemm__kernels__bf16_8c.html#aa17196850ccde558e4940c65151fb5b7", null ],
-    [ "gemm_backward_bf16_mixed", "gemm__kernels__bf16_8c.html#ac5d7318c45a162932cc6b2aa9f1ee463", null ],
     [ "gemm_bf16_fp32out", "gemm__kernels__bf16_8c.html#a8647bb8c6a9a965a92e1c98330f237af", null ],
     [ "gemm_blocked_serial_bf16", "gemm__kernels__bf16_8c.html#a9020b3311acc4db83cc7243b3d13d439", null ],
     [ "gemm_nn_bf16", "gemm__kernels__bf16_8c.html#ab1ff051870eb81a06b9d60bcda0c9399", null ],
-    [ "gemm_nt_bf16", "gemm__kernels__bf16_8c.html#a9e5a60d8bcbdf7ccf28b31142fdc624a", null ],
-    [ "gemm_nt_bf16_bf16_storage", "gemm__kernels__bf16_8c.html#ae0091c47ade010e18f3673ab97f50eb0", null ],
-    [ "gemm_tn_bf16", "gemm__kernels__bf16_8c.html#a2184db7c1d029252c677e9bc0c71d80d", null ],
-    [ "gemv_bf16", "gemm__kernels__bf16_8c.html#aafd2b9c7cd0a9cf473d4f893c9534b21", null ]
+    [ "gemm_tn_bf16", "gemm__kernels__bf16_8c.html#a2184db7c1d029252c677e9bc0c71d80d", null ]
 ];
