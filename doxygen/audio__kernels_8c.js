d0cf2aed8cf674d80a2318409f7630d79b1bdb9a
antshiv <antshiv@users.noreply.github.com>
2026-07-20 22:20:56 +0000

    deploy: c589475f3ecab0833c9ea9293c2bea61a5587f96

--- doxygen/audio__kernels_8c.js
+++ doxygen/audio__kernels_8c.js
@@ -1,6 +1,13 @@
 var audio__kernels_8c =
 [
     [ "CK_AUDIO_PI_F", "audio__kernels_8c.html#a6e7adb42c11df3ade2b6b770105e369b", null ],
+    [ "audio_conv1d_channel_major_f32", "audio__kernels_8c.html#adf8b1ff9727b956bc11157aa96051a62", null ],
+    [ "audio_pcm_s16_to_mono_f32", "audio__kernels_8c.html#a96aaf9c1b604b056eaca65a7c329a239", null ],
+    [ "audio_resample_linear_f32", "audio__kernels_8c.html#a09f214a1c9147bcc92e30edfde9daece", null ],
+    [ "audio_resampled_frame_count", "audio__kernels_8c.html#af899a2a47519d1740ab65706a5430e14", null ],
+    [ "audio_stft_power_precomputed_f32", "audio__kernels_8c.html#a085afc43b7b3f9cbf3f13003b081572d", null ],
+    [ "audio_stft_precompute_tables_f32", "audio__kernels_8c.html#a702f5960dd224ef9b8367e40c50e0859", null ],
+    [ "audio_transpose_channel_to_token_f32", "audio__kernels_8c.html#a9ec41be78d0845bf19362d4f5c66918d", null ],
     [ "audio_whisper_log_mel_from_power_reference_f32", "audio__kernels_8c.html#ab16ee955749f4f39bba1dad4b0c651e0", null ],
     [ "audio_whisper_log_mel_reference_f32", "audio__kernels_8c.html#a083aad385aaa9d460763421633528a9d", null ],
     [ "audio_whisper_stft_power_reference_f32", "audio__kernels_8c.html#a6cc87d2eaa6781543bc2730968241057", null ],
--- doxygen/ckernel__audio_8h.js
+++ doxygen/ckernel__audio_8h.js
@@ -4,6 +4,13 @@ var ckernel__audio_8h =
     [ "CK_AUDIO_WHISPER_N_FFT", "ckernel__audio_8h.html#a8104385637cb3f91a6e39939e0db5761", null ],
     [ "CK_AUDIO_WHISPER_POWER_BINS", "ckernel__audio_8h.html#afb106d375720b93e8d92279d2f9d4661", null ],
     [ "CK_AUDIO_WHISPER_SAMPLE_RATE", "ckernel__audio_8h.html#af1c758eba3f2e41f26558e4d9662aefa", null ],
+    [ "audio_conv1d_channel_major_f32", "ckernel__audio_8h.html#adf8b1ff9727b956bc11157aa96051a62", null ],
+    [ "audio_pcm_s16_to_mono_f32", "ckernel__audio_8h.html#a96aaf9c1b604b056eaca65a7c329a239", null ],
+    [ "audio_resample_linear_f32", "ckernel__audio_8h.html#a09f214a1c9147bcc92e30edfde9daece", null ],
+    [ "audio_resampled_frame_count", "ckernel__audio_8h.html#af899a2a47519d1740ab65706a5430e14", null ],
+    [ "audio_stft_power_precomputed_f32", "ckernel__audio_8h.html#a085afc43b7b3f9cbf3f13003b081572d", null ],
+    [ "audio_stft_precompute_tables_f32", "ckernel__audio_8h.html#a702f5960dd224ef9b8367e40c50e0859", null ],
+    [ "audio_transpose_channel_to_token_f32", "ckernel__audio_8h.html#a9ec41be78d0845bf19362d4f5c66918d", null ],
     [ "audio_whisper_log_mel_from_power_reference_f32", "ckernel__audio_8h.html#ab16ee955749f4f39bba1dad4b0c651e0", null ],
     [ "audio_whisper_log_mel_reference_f32", "ckernel__audio_8h.html#a083aad385aaa9d460763421633528a9d", null ],
     [ "audio_whisper_stft_power_reference_f32", "ckernel__audio_8h.html#a6cc87d2eaa6781543bc2730968241057", null ]
