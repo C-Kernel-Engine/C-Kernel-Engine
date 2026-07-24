728b918a57e1eceb42bbf207d59320cc03febee0
antshiv <antshiv@users.noreply.github.com>
2026-07-24 13:58:05 +0000

    deploy: affa4b55d40d37a2c7f5c804a94dc87f31ca607f

--- doxygen/gemm__kernels__q6k__q8k_8c.js
+++ doxygen/gemm__kernels__q6k__q8k_8c.js
@@ -1,7 +1,12 @@
 var gemm__kernels__q6k__q8k_8c =
 [
+    [ "ck_dot_q6_k_q8_k_fast_or_ref", "gemm__kernels__q6k__q8k_8c.html#a65fa62c32264072d07f5ff8a00ded586", null ],
+    [ "ck_q6_k_q8_k_provider_name", "gemm__kernels__q6k__q8k_8c.html#a4add2625b432613d83486a87bdfd5da5", null ],
+    [ "ck_q6k_q8k_force_ref", "gemm__kernels__q6k__q8k_8c.html#aa98a62f017969e334a0d3bbcbf34935d", null ],
     [ "dot_q6_k_q8_k_ref", "gemm__kernels__q6k__q8k_8c.html#ae26ffd50d4f4c9b317e265d436cdf9e1", null ],
     [ "gemm_nt_q6_k_q8_k", "gemm__kernels__q6k__q8k_8c.html#ac912d4edc9cd1c7de9f1697ae510fbff", null ],
+    [ "gemm_nt_q6_k_q8_k_tile", "gemm__kernels__q6k__q8k_8c.html#a0486ad5eb531ba02427e9765bab1d146", null ],
+    [ "gemm_nt_q6_k_q8_k_tiled", "gemm__kernels__q6k__q8k_8c.html#a930bfa3a27195fd66e4af6c6d617907e", null ],
     [ "gemm_q6_k_q8_k", "gemm__kernels__q6k__q8k_8c.html#a6f8eb04ad56d34cf1d38d49ea19a7639", null ],
     [ "gemv_q6_k_q8_k", "gemm__kernels__q6k__q8k_8c.html#a7b78875a957c11b30c9f7170b6c87c45", null ],
     [ "gemv_q6_k_q8_k_avx", "gemm__kernels__q6k__q8k_8c.html#a2267ad482f0cda1249db2f6d5273b5da", null ],
