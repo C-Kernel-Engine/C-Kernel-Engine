91950cdb5b353c8debe2c5298cf370a837ee40ad
antshiv <antshiv@users.noreply.github.com>
2026-07-26 06:10:48 +0000

    deploy: ef43b119bee3c3ce3947cd80372b96ffd6b1a801

--- doxygen/search/defines_11.js
+++ doxygen/search/defines_11.js
@@ -1,8 +1,6 @@
 var searchData=
 [
-  ['sample_5fnext_5ftoken_0',['SAMPLE_NEXT_TOKEN',['../ck__cli__v6_86_8c.html#ae495880372db70e28128c93942bb488f',1,'ck_cli_v6.6.c']]],
-  ['sliding_5fdecode_5fimpl_1',['SLIDING_DECODE_IMPL',['../attention__kernels__sliding_8c.html#a7d2f80b73d7c7a8c95cd87a05a6c8b8e',1,'attention_kernels_sliding.c']]],
-  ['sliding_5fdecode_5fimpl_5fgemma4_2',['SLIDING_DECODE_IMPL_GEMMA4',['../attention__kernels__sliding_8c.html#a5c5b435d153c3e7d24d113374c6a60ab',1,'attention_kernels_sliding.c']]],
-  ['sliding_5fflash_5fimpl_3',['SLIDING_FLASH_IMPL',['../attention__kernels__sliding_8c.html#ab21dd17029fc259dee0b11f03c1e2a1f',1,'attention_kernels_sliding.c']]],
-  ['sliding_5fflash_5fimpl_5fgemma4_4',['SLIDING_FLASH_IMPL_GEMMA4',['../attention__kernels__sliding_8c.html#aaffd946235fad0bbdf00158a14d39baf',1,'attention_kernels_sliding.c']]]
+  ['sample_5fnext_5ftoken_4907',['SAMPLE_NEXT_TOKEN',['../ck__cli__v6_86_8c.html#ae495880372db70e28128c93942bb488f',1,'ck_cli_v6.6.c']]],
+  ['sliding_5fdecode_5fimpl_4908',['SLIDING_DECODE_IMPL',['../attention__kernels_8c.html#a7d2f80b73d7c7a8c95cd87a05a6c8b8e',1,'attention_kernels.c']]],
+  ['sliding_5fflash_5fimpl_4909',['SLIDING_FLASH_IMPL',['../attention__kernels_8c.html#ab21dd17029fc259dee0b11f03c1e2a1f',1,'attention_kernels.c']]]
 ];
