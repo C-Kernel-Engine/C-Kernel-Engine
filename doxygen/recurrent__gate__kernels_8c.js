1551ee80b0c7c2c07dc247a5ef1cff397576d80b
antshiv <antshiv@users.noreply.github.com>
2026-07-27 17:13:45 +0000

    deploy: 8227a8ebc3d4cdf0ce742f593d7e4eb7e5ea2302

--- doxygen/recurrent__gate__kernels_8c.js
+++ doxygen/recurrent__gate__kernels_8c.js
@@ -1,5 +1,6 @@
 var recurrent__gate__kernels_8c =
 [
+    [ "_GNU_SOURCE", "recurrent__gate__kernels_8c.html#a369266c24eacffb87046522897a570d5", null ],
     [ "recurrent_dt_gate_backward", "recurrent__gate__kernels_8c.html#af3e6a26ed748383231350f849f4ed039", null ],
     [ "recurrent_dt_gate_expanded_forward", "recurrent__gate__kernels_8c.html#a50f8cc0388e79a27e195ef4e25a9e64f", null ],
     [ "recurrent_dt_gate_forward", "recurrent__gate__kernels_8c.html#af6f9c6f77b75d2ac115ba325fd0d618b", null ],
@@ -7,5 +8,7 @@ var recurrent__gate__kernels_8c =
     [ "recurrent_silu_backward", "recurrent__gate__kernels_8c.html#ae4d0d6861eb48e7e667044bfd536277d", null ],
     [ "recurrent_silu_forward", "recurrent__gate__kernels_8c.html#a8c4bf807b61bfd9add72843468cb7b05", null ],
     [ "recurrent_silu_forward_ggml", "recurrent__gate__kernels_8c.html#a68d1de609142507727987cabe36fe7a6", null ],
+    [ "recurrent_silu_forward_pytorch_bf16_input_fp32_output", "recurrent__gate__kernels_8c.html#a500cf70fc074fd75ae5b691103de1df3", null ],
+    [ "recurrent_silu_forward_pytorch_bf16_storage", "recurrent__gate__kernels_8c.html#aa9bb1f355b62b068ba37724e41083e20", null ],
     [ "recurrent_softplus", "recurrent__gate__kernels_8c.html#a55f0223c9af0726faa6b1bb572ba78bf", null ]
 ];
--- doxygen/recurrent__qk__norm__kernels_8c.js
+++ doxygen/recurrent__qk__norm__kernels_8c.js
@@ -2,6 +2,9 @@ var recurrent__qk__norm__kernels_8c =
 [
     [ "recurrent_l2_norm_rows_backward_one", "recurrent__qk__norm__kernels_8c.html#a0dd8cd71bd25ed57393abeea5eeec5c4", null ],
     [ "recurrent_l2_norm_rows_forward_one", "recurrent__qk__norm__kernels_8c.html#a93bfaa20cdf99a99fcf3fef56d39cd27", null ],
+    [ "recurrent_pytorch_bf16_l2_rows", "recurrent__qk__norm__kernels_8c.html#a3db433fc055482ec19e14c60e3a89da6", null ],
+    [ "recurrent_pytorch_bf16_square_sum", "recurrent__qk__norm__kernels_8c.html#aed1cf9fdbfe14ac17ad5060c8805b386", null ],
     [ "recurrent_qk_l2_norm_backward", "recurrent__qk__norm__kernels_8c.html#ac4af9b8924cf11e95ce1d4cbbf4e8d51", null ],
-    [ "recurrent_qk_l2_norm_forward", "recurrent__qk__norm__kernels_8c.html#a259410cd2b892b43fbdfe309cf0103aa", null ]
+    [ "recurrent_qk_l2_norm_forward", "recurrent__qk__norm__kernels_8c.html#a259410cd2b892b43fbdfe309cf0103aa", null ],
+    [ "recurrent_qk_l2_norm_pytorch_bf16_storage", "recurrent__qk__norm__kernels_8c.html#a1f8ded1d8bc4b06c825aceb06df8bbed", null ]
 ];
