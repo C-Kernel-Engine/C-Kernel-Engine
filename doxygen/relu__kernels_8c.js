f2dd67036575c5526398fa2708f97af2c6133b33
antshiv <antshiv@users.noreply.github.com>
2026-07-31 01:44:42 +0000

    deploy: c0a04690a6428d493f01179788cb6d5053e5d040

--- doxygen/relu__kernels_8c.js
+++ doxygen/relu__kernels_8c.js
@@ -1,5 +1,7 @@
 var relu__kernels_8c =
 [
+    [ "relu2_backward", "relu__kernels_8c.html#a7987de4b09bfb9f8d04405a6d9d2c4c5", null ],
+    [ "relu2_forward", "relu__kernels_8c.html#ab7af1607007954a2841bad370c7f9e35", null ],
     [ "relu_backward", "relu__kernels_8c.html#a434da7f0647cdada09cd6ad435560467", null ],
     [ "relu_forward", "relu__kernels_8c.html#a856216dca72f3534c079f429d1d58d48", null ],
     [ "relu_forward_inplace", "relu__kernels_8c.html#abbc4f192b13bed9ed1614c99cee2db91", null ]
