f859c03117b757772cd4230b126838b853497e70
antshiv <antshiv@users.noreply.github.com>
2026-08-03 21:56:22 +0000

    deploy: 62dad4c9c3c9cc5124e474e0fcdee5c02f1c76d4

--- doxygen/rmsnorm__kernels_8c.js
+++ doxygen/rmsnorm__kernels_8c.js
@@ -1,17 +1,5 @@
 var rmsnorm__kernels_8c =
 [
-    [ "gemma4_v_norm_forward", "rmsnorm__kernels_8c.html#abf3180347af2f640c551aa53af1878b8", null ],
     [ "rmsnorm_backward", "rmsnorm__kernels_8c.html#aca68d7dda1a7de9ff926920804751db0", null ],
-    [ "rmsnorm_backward_strict_scalar", "rmsnorm__kernels_8c.html#ac429025594088a6caf98de6d42095692", null ],
-    [ "rmsnorm_forward", "rmsnorm__kernels_8c.html#a9a068ed02d809a81a23f09e61b820baf", null ],
-    [ "rmsnorm_forward_fp64_sum", "rmsnorm__kernels_8c.html#a1a31e0326acc7c3cda2eb4013c23d701", null ],
-    [ "rmsnorm_forward_kv_lora", "rmsnorm__kernels_8c.html#ab929ece66f6bc3c22f4cd1562eff8089", null ],
-    [ "rmsnorm_forward_llama_production", "rmsnorm__kernels_8c.html#a21313e5d9f149097d8059085232aabe9", null ],
-    [ "rmsnorm_forward_no_weight", "rmsnorm__kernels_8c.html#a15bad3d2cc858e8f44ec48f7aa045b59", null ],
-    [ "rmsnorm_forward_pytorch_bf16_storage", "rmsnorm__kernels_8c.html#a84e08713e6234a99c0559a1538fa7a0c", null ],
-    [ "rmsnorm_forward_pytorch_bf16_storage_impl", "rmsnorm__kernels_8c.html#a85614673b1be567e0a1e828c64a7af70", null ],
-    [ "rmsnorm_forward_qwen3next_pytorch_bf16_storage", "rmsnorm__kernels_8c.html#a1eac561e5f84ca18385a9489e1a6d01b", null ],
-    [ "rmsnorm_forward_strict_scalar", "rmsnorm__kernels_8c.html#a6f5d41513aebcf5170ede8b64de0bea8", null ],
-    [ "rmsnorm_forward_strided_f32", "rmsnorm__kernels_8c.html#aae8ede0a82b914bbd6191d39c29b4e76", null ],
-    [ "rmsnorm_llama_production_rstd", "rmsnorm__kernels_8c.html#adc6b003cb5d9f1c2977a6b9b209b84b7", null ]
+    [ "rmsnorm_forward", "rmsnorm__kernels_8c.html#a9a068ed02d809a81a23f09e61b820baf", null ]
 ];
