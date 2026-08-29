1afd16042d65501efa98b4dc174356dc8b482ce6
antshiv <antshiv@users.noreply.github.com>
2026-08-29 03:12:48 +0000

    deploy: 71f11c08f09d2794adfa53dba63080209b245a0b

--- doxygen/navtreeindex25.js
+++ doxygen/navtreeindex25.js
@@ -1,5 +1,24 @@
 var NAVTREEINDEX25 =
 {
+"vision__kernels_8c.html#a38fa089663c3300b061508567cd18462":[2,0,1,1,90,12],
+"vision__kernels_8c.html#a3ffa58aab7da5c6e77096ad19385329a":[2,0,1,1,90,8],
+"vision__kernels_8c.html#a4a280c920122a804828475fb09571052":[2,0,1,1,90,14],
+"vision__kernels_8c.html#a6414bc6af9c28e7baf31b434073e794f":[2,0,1,1,90,5],
+"vision__kernels_8c.html#a6933897ffa7aa6a48b03e6bd01087ae0":[2,0,1,1,90,18],
+"vision__kernels_8c.html#a86aec042bcceb0bd8955f0daf0d986c4":[2,0,1,1,90,7],
+"vision__kernels_8c.html#a95f0081ec402d0df10805093d32c8ae5":[2,0,1,1,90,6],
+"vision__kernels_8c.html#a9af0dbbeb1b7fd100f99d796eaf0623f":[2,0,1,1,90,4],
+"vision__kernels_8c.html#abb267c5e63943557c83c193b2bd637fd":[2,0,1,1,90,16],
+"vision__kernels_8c.html#ac3987d3d95a62a769029100abbbf852a":[2,0,1,1,90,3],
+"vision__kernels_8c.html#ace7994d37be7b565c895e0b7205baad2":[2,0,1,1,90,10],
+"vision__kernels_8c.html#ae079db374514c445ebcfdd317dd3c15e":[2,0,1,1,90,1],
+"vision__kernels_8c.html#ae9b2c5016959bd797ba2729965604e64":[2,0,1,1,90,15],
+"vision__kernels_8c.html#aed5697928aadb8733b7bca52acfcef6e":[2,0,1,1,90,0],
+"vision__kernels_8c.html#aedf35588a71c8af25400e79faeec6dee":[2,0,1,1,90,9],
+"vision__kernels_8c.html#af0161c757898c6d0193bcae1415dc1e4":[2,0,1,1,90,21],
+"vision__kernels_8c_source.html":[2,0,1,1,90],
+"vision__kernels__bf16_8c.html":[2,0,1,1,91],
+"vision__kernels__bf16_8c.html#a57139580e075be2d5a65cfffc8e84898":[2,0,1,1,91,0],
 "vision__kernels__bf16_8c.html#aee95c2d2b45191471be617739a41ac0d":[2,0,1,1,91,1],
 "vision__kernels__bf16_8c_source.html":[2,0,1,1,91]
 };
